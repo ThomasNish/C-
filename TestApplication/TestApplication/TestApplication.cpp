6bd5850273063efdf357d7071c38477be462003e
Thomas Nishimura <tn44@evansville.edu>
2016-09-27 16:05:58 -0700

    Cacao

--- TestApplication/TestApplication/TestApplication.cpp
+++ TestApplication/TestApplication/TestApplication.cpp
@@ -1,14 +1,51 @@
-// TestApplication.cpp : Defines the entry point for the console application.
+//------------------------------------------------------------------
+//   File:        Stuff.c
 //
+//   Functions:
+//                main()               
+//-------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "stdafx.h"
-
-
-int main()
+//---------------------------------------------------------------------------------
+//   Function:    main()
+//
+//   Title:       Does nothing
+//
+//   Description: 
+//                Calculates and displays stuff
+//
+//   Programmer:  Thomas Nishimura
+//   
+//   Date:        9/22/16
+// 
+//   Version:     1.0
+//  
+//   Environment: Intel Xeon PC 
+//                Software:  OS: MS Windows 10  
+//                Compiles under Microsoft Visual Studio.Net 2015
+//
+//   Input: 	  stuff from Console
+//   Output:      Screen display of stuff
+//
+//   Calls:       
+// 
+//   Returns:     EXIT_SUCCESS (successful execution)
+//
+//   History Log:
+//                10/31/16  TN completed version 1.0
+//                3/28/05  TN completed version 1.1
+//                1/03/11  TN completed minor adjustments version 1.11
+// ------------------------------------------------------------------------------
+int main(void)
 {
+	int cacaoPercentage = 0;
+	float costInDollars = 0.0f;
 
+	printf("Please enter percentage of cacao desired: ");
+	printf("Percentage of cacao is %d%%\n", &cacaoPercentage);
+	// scanf("");
 
-
-    return 0;
+	getchar();
+	return EXIT_SUCCESS;
 }
-
