21dd94d1c5b667f73b0e69a7475b16641e09d454
Thomas Nishimura <tn44@evansville.edu>
2016-09-29 17:10:10 -0700

    Closer to completing project 1

--- Project1/Project1/Project1.cpp
+++ Project1/Project1/Project1.cpp
@@ -8,7 +8,17 @@
 #include <stdlib.h>
 #include <math.h>
 
-//-------------------------------------------------------------------------------------------
+const double MULTIPLIER = 100.0;
+const long long CHANGEINTWENTY = 2000;
+const long long CHANGEINTEN = 1000;
+const long long CHANGEINFIVE = 500;
+const long long CHANGEINONE = 100;
+
+const long long CHANGEINQUARTERS = 25;
+const long long CHANGEINDIMES = 10;
+const long long CHANGEINNICKLES = 5;
+const long long CHANGEINPENNIES = 1;
+//----------------------------------------------------------------------------
 //	Function: main
 //
 // Title: Project1
@@ -46,15 +56,92 @@
 //----------------------------------------------------------------------------
 int main(void)
 {
-	double amountPurchased = 0;
-	double amountTendered = 0;
+	double rounding(int amountgiven);
+
+	double amountPurchased = 0.00;
+	double amountTendered = 0.00;
+	double amountofChange = 0.00;
+	
+	long long numofTwenty = 0;
+	long long numofTen = 0;
+	long long numofFive = 0;
+	long long numOne = 0;
+
+	long long numofQuarter = 0;
+	long long numofDimes = 0;
+	long long numofNickles = 0;
+	long long numofPennies = 0;
 
 	printf("Welcome to Change Counter by Thomas Nishimura\n\n");
+
+	// Getting user input for amount purchased
 	printf("\tPlease enter the total amount of purchase(in dollars): $");
 	scanf_s("%lf", &amountPurchased);
-	printf("\tPlease enter amount of money tendered: $");
+	amountPurchased = rounding(amountPurchased);
+	printf("\t%.2lf", amountPurchased);
+
+	// Getting user input for amount of money paid
+	printf("\n\tPlease enter amount of money tendered: $");
 	scanf_s("%lf", &amountTendered);
-	//printf("%lf\n", amountPurchased);
+	amountTendered = rounding(amountTendered);
+	printf("\t%.2lf", amountTendered);
+	printf("\n");
+	
+	// Handling if user did not enter enough money
+	if (amountTendered < amountPurchased)
+		printf("You did not enter enough money");
+
+
+	amountofChange = amountPurchased - amountTendered;
+	printf("\n\t%.2lf", amountofChange);
+	
+	// Doing the calculation of change
+	numofTwenty = (int)amountofChange % CHANGEINTWENTY;
+	
+
+
 	getchar();
 	return EXIT_SUCCESS;
+}
+
+//----------------------------------------------------------------------------
+//	Function: rounding
+//
+// Title: Project1
+//
+//	Description: Gets user input and displays correct change
+// 
+//
+// Programmer: Thomas Nishimura
+// 
+// Date: 9/27/2016
+// Version: 1.0
+//
+//	Environment:
+//                     Hardware: Dell 
+//
+//                      Software: Windows 10, Visual Studios 2015
+//
+//	Input: Amount of payment
+//
+//	Output: none
+//
+//	Parameters: int amountgiven
+// 
+//
+// Returns: amount given
+// 
+// 
+//	Called By: main()
+// 
+//	Calls: n/a
+//
+//	History Log:
+//
+// 
+//----------------------------------------------------------------------------
+double rounding(int amountgiven)
+{
+	floor(amountgiven * MULTIPLIER + .5);
+	return amountgiven;
 }
