0043e1e278f6b5864ff52bb98e12a94be6af54b5
ThomasNish <Tn44@evansville.edu>
2016-10-06 17:24:17 -0700

    Hopefully everything works

--- Project1/Project1/Project1.cpp
+++ Project1/Project1/Project1.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
 
 //Declaring my constants
 const double MULTIPLIER = 100.0;
@@ -60,18 +61,17 @@ double rounding(double amountgiven);
 //----------------------------------------------------------------------------
 int main(void)
 {
-	
-	//double correctdecimal(int amount, int decimal);
-
+	// Declaring all of my double variables dealing with purchase and tendered
 	double amountPurchased = 0.00;
 	double amountTendered = 0.00;
 	double amountofChange = 0.00;
-	double amountofChangeinCents = 0.00;
+	long long amountofChangeinCents = 0;
 	double amounfofChangeDue = 0.00;
-	double amountofChangeDueinCents = 0.00;
+	long long amountofChangeDueinCents = 0;
 	double ABSofChange = 0.00;
 
-	int numofTwenty = 0;
+	// Declaring my int dealing with incrimental growth based off of change needed
+	long numofTwenty = 0;
 	int numofTen = 0;
 	int numofFive = 0;
 	int numOne = 0;
@@ -88,6 +88,17 @@ int main(void)
 	// Getting user input for amount purchased
 	printf("\tPlease enter the total amount of purchase(in dollars): $");
 	scanf_s("%lf", &amountPurchased);
+
+	/*
+	// Dealing with if users entered a letter anywhere in their input
+	if (isdigit((int)amountPurchased))
+		;
+	else
+	{
+		printf("\tYou entered a letter.\n");
+		return EXIT_FAILURE;
+	}
+	*/
 	amountPurchased = rounding(amountPurchased);
 	//amountPurchased = (((amountPurchased * MULTIPLIER) + 0.5) / MULTIPLIER);
 	printf("\t%.2lf", amountPurchased);
@@ -95,22 +106,35 @@ int main(void)
 	// Getting user input for amount of money paid
 	printf("\n\tPlease enter amount of money tendered: $");
 	scanf_s("%lf", &amountTendered);
+
+	/*
+	// Dealing with if users enter a letter anywhere in their input
+	if (isdigit((int)amountTendered))
+		;
+	else
+	{
+		printf("\tYou entered a letter.\n");
+		return EXIT_FAILURE;
+	}
+	*/
 	amountTendered = rounding(amountTendered);
 	printf("\t%.2lf\n", amountTendered);
 	amountofChange = (amountTendered - amountPurchased);
 
 	amounfofChangeDue = fabs(amountTendered - amountPurchased);
-	amountofChangeDueinCents = amounfofChangeDue * 100 + .5;
+	amountofChangeDueinCents = (long long)(amounfofChangeDue * 100 + .5);
 	ABSofChange = fabs(amountofChange);
-	// Handling if user did not enter enough money
+
+	// Handling if user did not enter enough money.
 	if (amountTendered < amountPurchased)
 	{
 		printf("\n\tYou did not enter enough money.");
 		printf("\n\tYou still owe $%.2lf\n", ABSofChange);
 
-		numofTwenty = amountofChangeDueinCents / CHANGEINTWENTY;
+		// Doing the calculation of amount due for each of the options given, then moving to the next lowest amount.
+		numofTwenty = (long)amountofChangeDueinCents / CHANGEINTWENTY;
 		leftovers = (int)amountofChangeDueinCents % CHANGEINTWENTY;
-		printf("\n\tTwenties : %d", numofTwenty);
+		printf("\n\tTwenties : %ld", numofTwenty);
 
 		numofTen = leftovers / CHANGEINTEN;
 		leftovers %= CHANGEINTEN;
@@ -142,14 +166,14 @@ int main(void)
 		return 0;
 	}
 
-	
+
 	printf("\n\t%.2lf", ABSofChange);
-	amountofChangeinCents = amountofChange * 100 + .5;
+	amountofChangeinCents = (long long)(amountofChange * 100 + .5);
 	printf("\n\t-------------------------------------------");
 
-	// Doing the calculation of change
-	numofTwenty = amountofChangeinCents / CHANGEINTWENTY;
-	leftovers = (int)amountofChangeinCents % CHANGEINTWENTY;
+	// Doing the calculation of change for each of the options given, then moving to the next lowest amount
+	numofTwenty = (long)amountofChangeinCents / CHANGEINTWENTY;
+	leftovers = amountofChangeinCents % CHANGEINTWENTY;
 	printf("\n\tTwenties : %d", numofTwenty);
 
 	numofTen = leftovers / CHANGEINTEN;
@@ -180,13 +204,14 @@ int main(void)
 	leftovers %= CHANGEINPENNIES;
 	printf("\n\tPennies  : %d", numofPennies);
 	// total change should be $36.41
-	
+
 
 	printf("\n\t-------------------------------------------");
 
 	printf("\nThank you for using Change Counter!\n");
 	// Keeping the window open
-	getchar();
+	while(getchar() != '\n')
+		getchar();
 	return EXIT_SUCCESS;
 }
 
@@ -230,5 +255,3 @@ double rounding(double amountgiven)
 {
 	return floor((amountgiven * MULTIPLIER) + 0.5) / (MULTIPLIER);
 }
-
-
